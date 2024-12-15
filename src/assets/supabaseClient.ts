97dd33a31a10f562998f6f5f7dbbbdc4dd6aaac4
Yudha Dwi Lesmana <yudha.dlesmana@gmail.com>
2024-12-15 20:39:57 +0700

    edit URL dan anonKey

--- src/assets/supabaseClient.ts
+++ src/assets/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Ganti dengan URL dan API Key proyek Supabase Anda
-const supabaseUrl = 'https://yywxqjnhowfhuqqlvcwu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5d3hxam5ob3dmaHVxcWx2Y3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNjIzMjYsImV4cCI6MjA0OTgzODMyNn0.lFJ9ajvM3iojGjdKVtqZgRNjg7G0Cq8R7NylE9Qpmec';
+const supabaseUrl = 'https://nsysxbfavdctxapbbwwy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeXN4YmZhdmRjdHhhcGJid3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNjg3MjYsImV4cCI6MjA0OTg0NDcyNn0.0bF3Alo7Jj6FPXizVMTHyN_bd-FT2LYAc1DyhQXhDhY';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
